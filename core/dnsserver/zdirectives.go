f21fa187383b7d1ba1c18ee617cb70e810cf1485
Miek Gieben <miek@miek.nl>
2020-07-28 16:39:35 +0200

    Move to new torrent lib
    Signed-off-by: Miek Gieben <miek@miek.nl>

--- core/dnsserver/zdirectives.go
+++ core/dnsserver/zdirectives.go
@@ -55,5 +55,5 @@ var Directives = []string{
 	"whoami",
 	"on",
 	"sign",
-	"torrent",
+	"bittorrent",
 }
--- plugin/torrent/torrent.go
+++
@@ -1,9 +0,0 @@
-package torrent
-
-// Torrent contains the file data that needs to be torrented.
-type Torrent struct {
-	dbfile string
-	dht    bool
-
-	stop chan struct{}
-}
